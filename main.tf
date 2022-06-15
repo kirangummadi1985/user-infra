1703b0343b543202372cd5665247d51004ad8cc4
kiran <kirangummadi1985@gmail.com>
2022-06-15 23:38:23 +0530

    Documentdb

--- main.tf
+++ main.tf
@@ -10,13 +10,29 @@ module "vpc" {
 module "elasticache" {
   depends_on          = [module.vpc]
   source              = "github.com/kirangummadi1985/tf-module-elasticache-redis.git"
-  ENGINE              = var.ENGINE
-  ENGINE_VERSION      = var.ENGINE_VERSION
-  NODE_TYPE           = var.NODE_TYPE
+  ENGINE              = var.EC_ENGINE
+  ENGINE_VERSION      = var.EC_ENGINE_VERSION
+  NODE_TYPE           = var.EC_NODE_TYPE
   ENV                 = var.ENV
   COMPONENT           = var.COMPONENT
-  NODE_COUNT          = var.NODE_COUNT
-  FAMILY              = var.FAMILY
+  NODE_COUNT          = var.EC_NODE_COUNT
+  FAMILY              = var.EC_FAMILY
+  VPC_ID              = module.vpc.VPC_ID
+  VPC_CIDR            = module.vpc.VPC_CIDR
+  SUBNET_IDS          = module.vpc.SUBNET_IDS
+}
+
+module "documentdb" {
+  depends_on          = [module.vpc]
+  source              = "github.com/kirangummadi1985/tf-module-documentdb.git"
+  ENGINE              = var.DOCDB_ENGINE
+  ENGINE_VERSION      = var.DOCDB_ENGINE_VERSION
+  NODE_TYPE           = var.DOCDB_NODE_TYPE
+  ENV                 = var.ENV
+  COMPONENT           = var.COMPONENT
+  NODE_COUNT          = var.DOCDB_NODE_COUNT
+  FAMILY              = var.DOCDB_FAMILY
+  SKIP_FINAL_SNAPSHOT = var.DOCDB_SKIP_FINAL_SNAPSHOT
   VPC_ID              = module.vpc.VPC_ID
   VPC_CIDR            = module.vpc.VPC_CIDR
   SUBNET_IDS          = module.vpc.SUBNET_IDS
